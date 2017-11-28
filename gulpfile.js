e72691c111220c3b16a8a4cb66b69670b7677354
Andrew Dean <andrewdean565@gmail.com>
2017-11-28 01:01:25 +0000

    improved gulp file to work better with a josn config for future use and changable src and dist folders

--- gulpfile.js
+++ gulpfile.js
@@ -1,62 +1,120 @@
+//----------- REQUIRES -----------//
+
+//gulp
 var gulp = require('gulp'),
-minify = require('gulp-minify'),
-watch = require('gulp-watch'),
-sass = require('gulp-sass'),
-concat = require('gulp-concat'),
-cleanCSS = require('gulp-clean-css');
-rename = require('gulp-rename');
+  minify = require('gulp-minify'),
+  watch = require('gulp-watch'),
+  sass = require('gulp-sass'),
+  concat = require('gulp-concat'),
+  cleanCSS = require('gulp-clean-css'),
+  rename = require('gulp-rename'),
+  gulpif = require('gulp-if');
 
-//dev
 
-var dev = "./src";
+var fs = require('fs');
 
-//pro
+//----------- VARIABLES -----------//
 
-var jsLoc = './htdocs/js/';
-var cssLoc = './htdocs/css/';
-var scssLoc = './scss/*.scss';
+//config file
+var config = JSON.parse(fs.readFileSync('./gulp-config.json'));
 
-var jsFiles = [
-  './node_modules/jquery/dist/jquery.min.js',
-  // './node_modules/angular/angular.min.js',
-  // './node_modules/angular-resource/angular-resource.min.js',
-  // './node_modules/angular-route/angular-route.min.js',
-  './node_modules/neo4j-driver/lib/browser/neo4j-web.min.js',
-  './node_modules/popper.js/dist/umd/popper.min.js',
-  './node_modules/bootstrap/dist/js/bootstrap.min.js',
-  dev+'/js/functions.js',
-  dev+'/js/forms.js',
-  dev+'/js/app.js'
-];
+//----------- TASKS -----------//
 
-var scssFiles = [
-    './node_modules/bootstrap/scss/**/*.scss',
-    dev+'/scss/*.scss'
-];
+//bundle js pkgs
+gulp.task('bundleJS_pkg', function () {bundle("pkgs", "js");});
 
-gulp.task('compilejs', function () {
-   return gulp.src(jsFiles)
-   .pipe(concat('bundle.js'))
-   .pipe(minify({ ext:{src:'.js', min:'.min.js'}}))
-   .pipe(gulp.dest(jsLoc))
+//bundle css pkgs
+gulp.task('bundleSCSS_pkg', function () {bundle("pkgs","css");});
+
+//bundle js src
+gulp.task('bundleJS_src', function () {bundle("src","js");});
+
+//bundle css src
+gulp.task('bundleSCSS_src', function () {bundle("src","css");});
+
+//bundle js all
+gulp.task('bundleJS_all', function () {bundleAll("js");});
+
+//bundle css all
+gulp.task('bundleCSS_all', function () {bundleAll("css");});
+
+//bundle all
+gulp.task('bundle_all', function () {
+  bundle("pkgs", "js");
+  bundle("pkgs", "css");
+  bundle("src", "js");
+  bundle("src", "css");
+  bundleAll("js");
+  bundleAll("css");
 });
 
-gulp.task('compilecss', function () {
-   return gulp.src(scssFiles)
-   .pipe(sass())
-   .pipe(concat('bundle.css'))
-   .pipe(gulp.dest(cssLoc))
-   .pipe(cleanCSS())
-   .pipe(rename('bundle.min.css'))
-   .pipe(gulp.dest(cssLoc))
+
+
+//watch
+gulp.task('watch', function () {
+  //js pkgs
+  gulp.watch(config.pkgs.js, ['bundleJS_pkg']);
+  //css pkgs
+  gulp.watch(config.pkgs.css, ['bundleSCSS_pkg']);
+  //js src
+  gulp.watch(config.src.js, ['bundleJS_src']);
+  //css src
+  gulp.watch(config.src.css, ['bundleSCSS_src']);
+  //js all
+  gulp.watch(createBundle("js"), ['bundleJS_all']);
+  //css all
+  gulp.watch(createBundle("css"), ['bundleCSS_all']);
 });
 
-gulp.task('js_watch', function () {
-    gulp.watch(jsFiles, ['compilejs']);
-});
+//default
+gulp.task('default', ['bundle_all', 'watch']);
 
-gulp.task('css_watch', function () {
-    gulp.watch(scssFiles, ['compilecss']);
-});
+//----------- FUNCTIONS -----------//
 
-gulp.task('default', ['compilejs', 'js_watch', 'compilecss', 'css_watch']);
+//bundle src and pkgs
+function bundle(kind, type){
+  console.log("bundle : " + kind + "|" + type);
+
+  //get js pkgs
+  return gulp.src(config[kind][type])
+  //catch error
+  .on('error', catchTaskError)
+  //if css convert from scss to css
+  .pipe(gulpif((type == "css"), sass()))
+  //bundle to one file
+  .pipe(concat(config.dist[kind] + '.' + type))
+  //if css save file and clean
+  .pipe(gulpif((type == "css"), gulp.dest(config.dist.loc + "/" + type)))
+  .pipe(gulpif((type == "css"), cleanCSS()))
+  .pipe(gulpif((type == "css"), rename(config.dist[kind] + '.min.' + type)))
+  //else js
+  .pipe(gulpif((type == "js"), minify({ ext:{src:('.' + type), min:('.min.' + type)}})))
+  //set bundle location
+  .pipe(gulp.dest(config.dist.loc + "/" + type))
+}
+
+//bundle all
+function bundleAll(type){
+  return gulp.src(createBundle(type))
+  .pipe(concat('bundle.' + type))
+  //if css save file and clean
+  .pipe(gulpif((type == "css"), gulp.dest(config.dist.loc + "/" + type)))
+  .pipe(gulpif((type == "css"), cleanCSS()))
+  .pipe(gulpif((type == "css"), rename("bundle" + '.min.' + type)))
+  //else js
+  .pipe(gulpif((type == "js"), minify({ ext:{src:('.' + type), min:('.min.' + type)}})))
+  .pipe(gulp.dest(config.dist.loc + "/" + type))
+}
+
+function createBundle(type){
+  var bundle = [
+    config.dist.loc + "/" + type + "/" + config.dist.pkgs + ".min." + type,
+    config.dist.loc + "/" + type + "/" + config.dist.src + ".min." + type
+  ];
+  return bundle;
+}
+
+function catchTaskError(e){
+  console.log(error.toString())
+  this.emit('end')
+}
---
+++ htdocs/js/src.js
@@ -0,0 +1,128 @@
+
+var departments = new Vue({
+  el : '#e_dep',
+  data : {
+    departments: [],
+  },
+  created : function(){
+    this.getDepartments();
+  },
+  methods : {
+    getDepartments: function(){
+      this.$http.get(createUrl('departments')).then(response => {
+        this.departments = response.body;
+        console.log(this.departments);
+      }, response => {
+        //error
+      }).bind(this);
+    },
+  }
+});
+
+//---------ADD EMPLOYEE---------//
+$("#addEmployeeForm").submit(function(e){
+  e.preventDefault();
+  var form = document.getElementById("addEmployeeForm");
+  var data = new FormData(form);
+  submitForm(formToArray(data), 'addEmployee');
+});
+
+//---------ADD DEPARTMENT---------//
+$("#addDepartmentForm").submit(function(e){
+  e.preventDefault();
+  var form = document.getElementById("addDepartmentForm");
+  var data = new FormData(form);
+  submitForm(formToArray(data), 'addDepartment');
+});
+
+//---------VARIABLES---------//
+var showDebug = true;
+
+var codes = {
+  'success' : 1,
+  'dbError' : 2,
+  'inputError' : 3,
+  'unknown' : 4
+}
+
+var server = {
+  'protocol' : 'http://',
+  'ip' : '127.0.0.1',
+  'port' : '1337'
+}
+
+var departments, employees;
+
+//---------FUNCTIONS---------//
+
+//---------DEBUG LOGGING---------//
+function logDebug(type, text){if(showDebug){console.log(type + ' : ' + text);}}
+
+//---------SUBMITTING FORMS---------//
+function submitForm(data, page){
+  var loc = createUrl(page);
+  $.post( loc, data).done(function( data ) {
+    switch (data) {
+      case codes.success:
+        alert('record added');
+        $('#' + page + 'Form')[0].reset();
+        $('#' + page).modal('hide');
+        break;
+      case codes.inputError:
+        alert('there was an input error');
+        break;
+      case codes.dbError:
+        alert('there was an database error');
+        break;
+      case codes.unknown:
+        alert('there was an unkown error');
+        break;
+      default:
+    }
+  }).fail(function(error) {
+    alert('there was a url error');
+    console.log("error");
+  });
+}
+
+//---------GET DATA---------//
+function getData(page, type, data, trigger){
+  var loc = createUrl(page);
+  $.get( loc, data).done(function( data ) {
+    switch (data) {
+      case codes.inputError:
+        alert('there was an input error');
+        break;
+      case codes.dbError:
+        alert('there was an database error');
+        break;
+      case codes.unknown:
+        alert('there was an unkown error');
+        break;
+      default:
+      $(document).trigger('' + trigger + '', [data]);
+      return data;
+    }
+  }).fail(function(error) {
+    alert('there was a url error');
+    console.log("error");
+  });
+}
+
+//---------EXTRACT DATA---------//
+function extractData(data){
+  props = data._fields;
+}
+
+
+//---------CREATE URL---------//
+function createUrl(page){
+  return (server.protocol + server.ip + ":" + server.port + "/" + page);
+}
+
+//---------CONVERT FORM OBJECT TO ARRAY---------//
+function formToArray(form){
+    var arr = {};
+    for(var pair of form.entries()) {arr[pair[0]] = pair[1];}
+    return arr;
+}
